863702bc5033c748fff303c0b27eb9161fe8673b
mabdul6492 <mandul6492@gmail.com>
2020-03-18 12:48:40 +0530

    teacher added

--- ultron.py
+++ ultron.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 import pyttsx3
 import speech_recognition as sr
@@ -9,7 +10,7 @@ engine.setProperty('volume', 1)
 
 
 def speak(audio):
-    print('\nUltron : '+audio)
+    print('\nUltron : ' + audio)
     engine.say(audio)
     engine.runAndWait()
 
@@ -36,20 +37,61 @@ def take_command():
 
     try:
         print('recognizing....')
-        query = r.recognize_google(audio, language='en-IN')
-        speak(query)
+        response = r.recognize_google(audio, language='en-IN')
+        speak(response)
     except Exception as e:
         print(e)
         print('Say that again please...')
-        return 'None'
-    return query
+        response = take_command()
+        return response
+    return response.lower()
+
+
+def teacher(alphabet_objects):
+    alphabet = random.choice('abcdefghijklmnopqrstuvwxyz')
+    alphabet_objects = random.choice(alphabet_objects[alphabet])
+    sentence = f'{alphabet} for {alphabet_objects}'
+    speak(sentence)
+    return sentence.lower()
 
 
 if __name__ == '__main__':
-
+    alphabet_objects = {
+        'a': ['alligator', 'ambulance', 'anchor', 'ant', 'anteater', 'apple', 'aquarium', 'astronaut'],
+        'b': ['balloon', 'baker', 'bat', 'bear', 'bee', 'boat', 'broccoli', 'butterfly'],
+        'c': ['candle', 'car', 'cat', 'clown', 'cow', 'caterpillar', 'crab', 'crocodile'],
+        'd': ['daisy', 'deer', 'dinosaur', 'dog', 'dolphin', 'donkey', 'dove', 'duck'],
+        'e': ['egg', 'elephant', 'elk', 'envelope', 'eagle', 'earwig', 'easel', 'eel'],
+        'f': ['farmer', 'fire', 'fish', 'five', 'flag', 'flower', 'frog', 'fruit'],
+        'g': ['garden', 'gift', 'goat', 'goose', 'gorilla', 'grapes', 'grasshopper', 'groundhog'],
+        'h': ['hare', 'heart', 'hen', 'hippo', 'horse', 'house'],
+        'i': ['igloo', 'iguana', 'ink', 'insect'],
+        'j': ['jar', 'jeep', 'jellyfish', 'jester', 'jet'],
+        'k': ['king', 'kite', 'kitten', 'koala'],
+        'l': ['ladybug', 'lamb', 'leaf', 'lily', 'lion', 'lobster'],
+        'm': ['monkey', 'moon', 'moose', 'mouse'],
+        'n': ['nest', 'nine', 'numbers', 'nurse'],
+        'o': ['octopus', 'ostrich', 'otter', 'ox', 'oak', 'ocean', 'oval', 'overalls'],
+        'p': ['panda', 'penguin', 'pig', 'pumpkin'],
+        'q': ['quail', 'queen', 'quetzal', 'quilt'],
+        'r': ['rabbit', 'raccoon', 'rainbow', 'rat', 'reindeer', 'rooster'],
+        's': ['sand dollar', 'snail', 'snake', 'snowman', 'spider', 'starfish'],
+        't': ['tiger', 'toad', 'tractor', 'tree', 'turkey', 'turtle'],
+        'u': ['unicorn', 'universe', 'unicycle', 'utensil'],
+        'v': ['van', 'violin', 'volcano', 'vulture'],
+        'w': ['walrus', 'watermelon', 'wolf', 'woodpecker'],
+        'x': ['X-ray', 'xylophone', 'ox', 'fox'],
+        'y': ['yacht', 'yak', 'yarn', 'yoyo'],
+        'z': ['zebra', 'zigzag', 'zipper', 'zoo'],
+    }
     wish_me()
 
     while True:
-        take_command()
+        sentence = teacher(alphabet_objects)
+        response = take_command()
+        if sentence == response:
+            speak('You are Correct')
+        else:
+            speak('You are not Correct')
         if 'q' == input('\nEnter "Q" to Exit or press any other key : ').lower():
             break
